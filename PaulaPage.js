6b96c1129b91af983d7ad47a4616036027c275e8
Krzysztof Tucholski <krzysztof.tucholski@gc.dev>
2022-01-17 15:56:32 +0100

    zbawca Kris

--- PaulaPage.js
+++ PaulaPage.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 
 //const { result } = require('lodash');
 //const { render } = require('express/lib/response');
-const blogRoutes = require('../routes/blogRoutes');
+const blogRoutes = require('./routes/blogRoutes');
 
 
 
